9bc9099affafebd9edffa9b7a178b37da6f0af2f
Maciej Tomczyk <tomczyk.maciekk@gmail.com>
2018-08-06 17:40:29 +0200

    Fix unused import

--- xmlstats/src/integration-test/java/com/mt/xmlstats/XmlStatsIT.java
+++ xmlstats/src/integration-test/java/com/mt/xmlstats/XmlStatsIT.java
@@ -1,6 +1,5 @@
 package com.mt.xmlstats;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mt.xmlstats.mapper.StatsMapper;
 import com.mt.xmlstats.model.UrlWrapper;
